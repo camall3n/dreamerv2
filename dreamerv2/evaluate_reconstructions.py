d01456df187e7c1df490c694d7d3664808b0af00
Shreyas Raman <shreyassraman@gmail.com>
2022-09-24 17:07:13 +0400

    fixed indentation issue

--- dreamerv2/evaluate_reconstructions.py
+++ dreamerv2/evaluate_reconstructions.py
@@ -144,7 +144,7 @@ def main():
         agnt = agent.Agent(config, obs_space, act_space, step)
         #pdb.set_trace()
         agnt.load(saved_model)
-	pdb.set_trace()
+        pdb.set_trace()
         
         traject_list = driver(random_agent, train_step)
 
